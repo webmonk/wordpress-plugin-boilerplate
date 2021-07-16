f1d6bc394d4f5139b4fa3b4e2ad555e0a41222d1
Isaac Oyelowo <isaac@concept-gaming.com>
2021-07-16 02:26:17 +0100

    Modify readme, add languages and refactor plugin php file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wordpress-plugin-boilerplate
 A Wordpress plugin boilerplate to bootstrap Wordpress plugin development.
 
+In the language files, remember to rename the mo and po files with your plugin short name
+
--- plugin.php
+++ plugin.php
@@ -8,20 +8,45 @@ Author: Isaac Oyelowo
 Author URI: http://isaacoyelowo.dev
 */
 
-class Plugin_Class{
+class Plugin_Class {
+
+    public $pluginShortName;
 
     public function __construct () {
 
+        $this->pluginShortName = 'plugin';
         $this->addActions();
         $this->addFilters();
+        register_activation_hook( __FILE__, array( $this, 'onActivate' ) );
     }
 
     public function addActions() {
+
+        if ( is_admin() ) {
+
+            // Add only admin specific actions here
+        }
 
     }
 
     public function addFilters() {
 
+        if ( is_admin() ) {
+
+            // Add only admin specific filters here
+        }
+
+    }
+
+    public function onActivate() {
+
+        // Run this on plugin activation
+    }
+
+    public function localize() {
+
+        // Localization
+        load_plugin_textdomain(  $this->pluginShortName , false, dirname(plugin_basename(__FILE__)). "/languages" );
     }
 
 }
